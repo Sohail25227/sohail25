fbc45932fa160a9ec058e66c734589135691e650
sohail25227 <ahmad.sohail252002@gmail.com>
2021-02-22 21:35:41 +0530

    my portfolio

---
+++ app.js
@@ -0,0 +1,14 @@
+let hamburger = document.querySelector('.hamburger');
+    let times = document.querySelector('.times');
+    let mobileNav = document.querySelector('.mobile-nav');
+
+    hamburger.addEventListener('click',function(){
+    mobileNav.classList.add('open');
+
+
+    });
+    times.addEventListener('click',function(){
+        mobileNav.classList.remove('open');
+
+    });
+
