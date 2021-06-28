87e2b6fca833652074fdaa18d930d991432da39c
SOHAIL AHMAD <76154338+Sohail25227@users.noreply.github.com>
2021-06-28 12:53:13 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -12,3 +12,40 @@ let hamburger = document.querySelector('.hamburger');
 
     });
 
+    $(window).scroll(function() {
+        // if($(window).scrollTop()) {
+        //   $(".navbar").removeClass("navbar-dark");
+        //   $(".navbar").addClass("sticky navbar-light");
+        //   $("#myBtn").css("display", "block");
+        // } else {
+        //   $(".navbar").addClass("navbar-dark");
+        //   $(".navbar").removeClass("sticky navbar-light");
+        //   $("#myBtn").css("display", "none");
+        // }
+        if($(window).scrollTop()>1020&& $(window).scrollTop()<5000) {
+        console.log("1");
+          $(".procpp").addClass("cpp");
+          $(".prohtml").addClass("html");
+          $(".prodsa").addClass("dsa");
+          $(".proazure").addClass("azure");
+          
+        }else {
+          $(".procpp").removeClass("cpp");
+          $(".prohtml").removeClass("html");
+          $(".prodsa").removeClass("dsa");
+          $(".proazure").removeClass("azure");
+    
+        }
+      
+        // if($(window).scrollTop()>2000&& $(window).scrollTop()<7000) {
+        //   $("li.left").addClass("leftbefore");
+        //   $("li.right").addClass("rightbefore");
+        // } else {
+        //   $("li.left").removeClass("leftbefore");
+        //   $("li.right").removeClass("rightbefore");
+        // }
+      });
+    
+
+    
+
